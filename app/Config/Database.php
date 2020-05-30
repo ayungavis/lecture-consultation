2b69c623c8611efe2922fd2c1d117b2af741bf38
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-05-30 16:01:16 +0700

    Fix database configuration error

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => '',
+		'database' => 'qrcode',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
