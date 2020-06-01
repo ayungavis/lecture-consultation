4ecba4b191bdf7264a1f665c2f72e19d1d70657f
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-06-01 09:32:51 +0700

    Change base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://192.168.100.8:8081/';
+	public $baseURL = 'http://localhost:8080/';
 
 	/*
 	|--------------------------------------------------------------------------
